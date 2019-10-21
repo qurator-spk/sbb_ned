c17ebcd948c7491665ad47cfe8ad76e8d125477d
Kai Labusch <labusch@gmail.com>
2019-10-21 08:52:50 +0200

    remove memory problem

--- qurator/sbb_ned/index.py
+++ qurator/sbb_ned/index.py
@@ -93,6 +93,8 @@ def build(all_entities, embeddings, ent_type, n_trees, processes=10, distance_me
     ann_index = 0
     for res in run(get_embed_tasks(all_entities.loc[all_entities.TYPE == ent_type], split_parts), processes=processes,
                    initializer=EmbedTask.initialize, initargs=(embeddings,)):
+        
+        del embeddings
 
         title = res['title']
 
