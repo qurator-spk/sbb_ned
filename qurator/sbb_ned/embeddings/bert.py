7642857d26324ca9618c5a7d8a96a629df65f9f9
Kai Labusch <Kai.Labusch@sbb.spk-berlin.de>
2020-02-17 13:31:21 +0100

    add correct file

---
+++ qurator/sbb_ned/embeddings/bert.py
@@ -0,0 +1,50 @@
+from ..embeddings.base import Embeddings
+
+from flair.embeddings import Sentence
+
+class BertEmbeddings(Embeddings):
+
+    def __init__(self, model_path,
+                 layers="-1, -2, -3, -4", pooling_operation='first', use_scalar_mix=True, *args, **kwargs):
+
+        super(BertEmbeddings, self).__init__(*args, **kwargs)
+
+        self._path = model_path
+        self._embeddings = None
+        self._layers = layers
+        self._pooling_operation = pooling_operation
+        self._use_scalar_mix = use_scalar_mix
+
+    def get(self, keys):
+
+        if self._embeddings is None:
+
+            from flair.embeddings import BertEmbeddings
+
+            self._embeddings = BertEmbeddings(bert_model_or_path=self._path,
+                                              layers=self._layers,
+                                              pooling_operation=self._pooling_operation,
+                                              use_scalar_mix=self._use_scalar_mix)
+
+        sentences = [Sentence(key) for key in keys]
+
+        # noinspection PyUnresolvedReferences
+        self._embeddings.embed(sentences)
+
+        for s_idx, sentence in enumerate(sentences):
+
+            for t_idx, token in enumerate(sentence):
+
+                yield token, token.embedding.cpu().numpy()
+
+    def config(self):
+
+        return {'description': self.description()}
+
+    def description(self):
+
+        layer_str = self._layers
+        layer_str.strip()
+        layer_str = layer_str.replace(',', '_')
+
+        return "bert-layers_{}-pooling_{}-scalarmix_{}".format(layer_str, self._pooling_operation, self._use_scalar_mix)
