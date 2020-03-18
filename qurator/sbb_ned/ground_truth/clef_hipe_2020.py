3f7fa3b19ea7e8cf0500fa3ba39e39d11eb0bc88
Kai <kai@mynetmapper.org>
2020-03-18 14:09:28 +0100

    code beautification

--- qurator/sbb_ned/ground_truth/clef_hipe_2020.py
+++ qurator/sbb_ned/ground_truth/clef_hipe_2020.py
@@ -4,10 +4,7 @@ from io import StringIO
 import pandas as pd
 
 
-@click.command()
-@click.argument('clef-file', type=click.Path(exists=True), required=True, nargs=1)
-@click.argument('tsv-file', type=click.Path(), required=True, nargs=1)
-def clef2tsv(clef_file, tsv_file):
+def read_clef(clef_file):
 
     with open(clef_file, 'r') as f:
 
@@ -15,6 +12,23 @@ def clef2tsv(clef_file, tsv_file):
         part = []
         header = None
         urls = []
+
+        def make_part():
+
+            nonlocal part, parts
+
+            if len(part) == 0:
+                return
+
+            tmp = pd.read_csv(StringIO(header + "".join(part)), sep='\t', comment='#')
+
+            tmp = tmp.reset_index().rename(columns={'index': 'TOKEN_ID'})
+
+            tmp['url_id'] = len(parts)
+
+            parts.append(tmp)
+
+            part = []
 
         for line in f:
 
@@ -29,46 +43,32 @@ def clef2tsv(clef_file, tsv_file):
 
                 urls.append(line)
 
-                if len(part) == 0:
-                    continue
+                make_part()
 
-                df = pd.read_csv(StringIO(header + "".join(part)), sep='\t', comment='#')
+        make_part()
 
-                df = df.reset_index().rename(columns={'index': 'TOKEN_ID'})
+        return urls, pd.concat(parts).reset_index(drop=True)
 
-                df['url_id'] = len(parts)
 
-                parts.append(df)
+@click.command()
+@click.argument('clef-file', type=click.Path(exists=True), required=True, nargs=1)
+@click.argument('tsv-file', type=click.Path(), required=True, nargs=1)
+def clef2tsv(clef_file, tsv_file):
 
-                part = []
+    out_columns = ['No.', 'TOKEN', 'NE-TAG', 'NE-EMB', 'ID', 'url_id', 'left', 'right', 'top', 'bottom']
 
-        if len(part) > 0:
+    entity_types = ['PER', 'LOC', 'ORG', 'O']
 
-            df = pd.read_csv(StringIO(header + "".join(part)), sep='\t', comment='#')
-
-            df = df.reset_index().rename(columns={'index': 'TOKEN_ID'})
-
-            df['url_id'] = len(parts)
-
-            parts.append(df)
-
-    df = pd.concat(parts).reset_index(drop=True)
+    urls, df = read_clef(clef_file)
 
     df['NE-COARSE-LIT'] = df['NE-COARSE-LIT'].str[0:5].str.upper()
 
     df['NE-NESTED'] = df['NE-NESTED'].str[0:5].str.upper()
 
-    df['left'] = 0
-    df['right'] = 0
-    df['top'] = 0
-    df['bottom'] = 0
-
-    out_columns = ['No.', 'TOKEN', 'NE-TAG', 'NE-EMB', 'ID', 'url_id', 'left', 'right', 'top', 'bottom']
+    df['left'] = df['right'] = df['top'] = df['bottom'] = 0
 
     df = df.rename(columns={'TOKEN_ID': 'No.', 'TOKEN': 'TOKEN', 'NE-COARSE-LIT': 'NE-TAG', 'NE-NESTED': 'NE-EMB',
                             'NEL-LIT': 'ID', })
-
-    entity_types = ['PER', 'LOC', 'ORG', 'O']
 
     df.loc[~df['NE-TAG'].str[2:5].isin(entity_types), 'NE-TAG'] = 'O'
     df.loc[~df['NE-EMB'].str[2:5].isin(entity_types), 'NE-EMB'] = 'O'
@@ -77,7 +77,7 @@ def clef2tsv(clef_file, tsv_file):
 
     pd.DataFrame([], columns=out_columns).to_csv(tsv_file, sep="\t", quoting=3, index=False)
 
-    for (id, part), url in zip(df.groupby('url_id', sort=False, as_index=False), urls):
+    for (_, part), url in zip(df.groupby('url_id', sort=False, as_index=False), urls):
 
         with open(tsv_file, 'a') as f:
             f.write(url)
