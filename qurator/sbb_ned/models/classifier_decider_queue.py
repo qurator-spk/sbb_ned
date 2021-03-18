bb95d803fdee21f1bb0189df2a20fd387ef6124c
Kai <kai@mynetmapper.org>
2021-03-18 20:05:29 +0100

    --

--- qurator/sbb_ned/models/classifier_decider_queue.py
+++ qurator/sbb_ned/models/classifier_decider_queue.py
@@ -32,9 +32,6 @@ class ClassifierTask:
 
     def __call__(self, *args, **kwargs):
 
-        if self._entity_id is None:
-            return None, None, None
-
         all_input_ids = torch.tensor([f.input_ids for f in self._features], dtype=torch.long)
         all_input_mask = torch.tensor([f.input_mask for f in self._features], dtype=torch.long)
         all_segment_ids = torch.tensor([f.segment_ids for f in self._features], dtype=torch.long)
@@ -157,14 +154,12 @@ class ClassifierDeciderQueue:
 
             print("get_classifier_tasks: {}".format(entity_id))
 
-            if entity_id is None:
-
-                yield ClassifierTask(None, None, None)
-
             if len(candidates) == 0:
                 continue
 
             yield ClassifierTask(entity_id, features, candidates)
+
+        print('classifier tasks done')
 
     def get_decider_tasks(self):
 
